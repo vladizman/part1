2d85a02c64cffe78352c95ab5669d8b3f13fa7dd
Vlad Izman <vladizmanq@gmail.com>
2025-01-03 15:41:53 +0200

    ex 1.12 - 1.14

---
+++ anecdotes/anecdotes/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
+  console.log(votes);
+
+  const handleNextAnecdote = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+    console.log(randomIndex);
+    setSelected(randomIndex);
+  };
+
+  const handleVote = () => {
+    const votesCopy = [...votes];
+    votesCopy[selected] += 1;
+    setVotes(votesCopy);
+  };
+  return (
+    <div>
+      <h1>Anecdote of the Day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>Has Votes: {votes[selected]}</p>
+      <button onClick={handleVote}>Vote</button>
+      <button onClick={handleNextAnecdote}>Next Anecdote</button>
+
+      <h1>Anecdote with Most Votes</h1>
+      {votes.some((vote) => vote > 0) ? (
+        <p>{anecdotes[votes.indexOf(Math.max(...votes))]}</p>
+      ) : (
+        <p>No votes yet!</p>
+      )}
+    </div>
+  );
+};
+
+export default App;
